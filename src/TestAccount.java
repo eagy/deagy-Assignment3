efa0d5e96c4baedd01ebe1efd42105f0b1408232
Daniel Eagy <daniel.eagy@gmail.com>
2016-03-31 14:28:49 -0700

    Testing Phase 1

--- src/TestAccount.java
+++ src/TestAccount.java
@@ -1,9 +1,16 @@
+/**
+ * 
+ * @author Daniel Eagy
+ * @version 1.0
+ */
+
+import java.util.*;
 
 public class TestAccount {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		Scanner kb = new Scanner(System.in);
 		
 	}
 
